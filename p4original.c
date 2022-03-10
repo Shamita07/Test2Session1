6ec3770276a56b7dff570a3d35d0a2f7831bad0b
Shamita V <96277671+Shamita07@users.noreply.github.com>
2022-03-10 17:23:32 +0000

    p4done

---
+++ p4original.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+int input_number()
+{
+  int a;
+  printf("enter the number of fibonacci sequence: \n");
+  scanf("%d",&a);
+  return a;
+}
+
+int find_fibo(int n,int fi[n])
+{
+  
+  fi[1]=0;
+  fi[2]=1;
+  for(int i=3;i<=n;i++)
+    {
+      fi[i]=fi[i-1]+fi[i-2];
+    }
+  return fi[n]; 
+}
+void output(int n,int fibo)
+{
+  printf("the %d th fibonic number is %d",n,fibo);
+}
+
+int main()
+{
+  int n;
+  n=input_number();
+  int fibonic[n];
+  find_fibo(n,fibonic);
+  output(n,fibonic[n]);
+  return 0;
+}
